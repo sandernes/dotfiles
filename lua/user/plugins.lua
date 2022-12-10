9c51fe5b2d3e3019037929e0c34a39fbcda83711
Sander Ernes <sander.ernes@gmail.com>
2022-12-10 19:59:19 +0200

    Add rainbow parentheses for treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,7 @@ return packer.startup(function(use)
 
   -- Treesitter
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use { "p00f/nvim-ts-rainbow" }  -- rainbow parentheses
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
