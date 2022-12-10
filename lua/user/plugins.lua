3b2bf0be2273b8b57dd28b37048097a00ff5db57
Sander Ernes <sander.ernes@gmail.com>
2022-12-10 18:19:46 +0200

    Add nvim specific lua completions

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -53,6 +53,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lua" -- nvim specific Lua completions
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
