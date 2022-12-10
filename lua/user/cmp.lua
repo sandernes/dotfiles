e17431e1c95dc4f79a4d7ab2bf8763da68039e47
Sander Ernes <sander.ernes@gmail.com>
2022-12-10 18:20:41 +0200

    Add LSP

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -110,6 +110,7 @@ cmp.setup {
     end,
   },
   sources = {
+    { name = "nvim_lsp" },
     { name = "nvim_lua" },
     { name = "luasnip" },
     { name = "buffer" },
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,10 +54,18 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lua" -- nvim specific Lua completions
+  use "hrsh7th/cmp-nvim-lsp"  -- LSP completions
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- external tooling
+  use "williamboman/mason.nvim" -- neovim external tooling package manager
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
