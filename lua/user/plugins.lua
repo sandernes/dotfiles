70b799be8d82d3dd5c6c3804f966d06fceb5547c
Sander Ernes <sander.ernes@gmail.com>
2022-12-11 09:05:27 +0200

    Add gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,6 +81,9 @@ return packer.startup(function(use)
   -- Comments
   use "numToStr/Comment.nvim"
 
+  -- git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
