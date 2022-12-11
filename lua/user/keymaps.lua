a66af92c7a4e705e3c618889f029e2e35a4c06a1
Sander Ernes <sander.ernes@gmail.com>
2022-12-11 13:41:56 +0200

    Add toggleterm

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -57,13 +57,6 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
--- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
 -- Telesscope --
 keymap("n", "<c-p>", "<cmd>Telescope find_files<cr>", opts)
 --keymap("n", "<c-p>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = true }))<cr>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,9 @@ return packer.startup(function(use)
   -- statusline
   use "nvim-lualine/lualine.nvim"
 
+  -- toggleterm
+  use "akinsho/toggleterm.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
