a041fbe5b7fb9538ce6a24563399a34ec73130bf
Sander Ernes <sander.ernes@gmail.com>
2022-12-12 17:56:38 +0200

    Display block cursor in all modes

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -38,6 +38,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  guicursor = "n-v-c-i:block",             -- show block cursor in all modes
   textwidth = 80,
   colorcolumn = "+1",                      -- highlight 81th column
   formatoptions = "qrn1",
