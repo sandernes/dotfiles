31e96f88fe115dbad69dc3278ea8d32070038b26
Sander Ernes <sander.ernes@gmail.com>
2022-12-13 08:10:48 +0200

    Use unique names and remove close icons in bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -61,14 +61,14 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = false, -- no need for tab indicators in tab mode
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
+    enforce_regular_tabs = false,
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
