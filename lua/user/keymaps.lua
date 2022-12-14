706f8636130c47ad3f98bf82325226860c911d44
Sander Ernes <sander.ernes@gmail.com>
2022-12-14 19:02:04 +0200

    Add keymaps to save buffer and close window

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -30,6 +30,10 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- quit & save
+keymap("n", "zz", ":update<cr>", opts)
+keymap("n", "<C-q>", ":close<cr>", opts)
+
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
