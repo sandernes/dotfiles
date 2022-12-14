7e5009626c13ba470cbc9651f014eee37934adfa
Sander Ernes <sander.ernes@gmail.com>
2022-12-14 20:00:10 +0200

    Reformat

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -78,13 +78,13 @@ telescope.setup {
     },
   },
   -- pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+  -- Default configuration for builtin pickers goes here:
+  -- picker_name = {
+  --   picker_config_key = value,
+  --   ...
+  -- }
+  -- Now the picker_config_key will be applied every time you call this
+  -- builtin picker
   -- },
   extensions = {
     -- Your extension configuration goes here:
